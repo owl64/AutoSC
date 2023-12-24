64d4842e07762075eea128f245705f544835b49a
owl64 <hermawandi64@gmail.com>
2023-12-24 22:10:20 +0700

    modified:   update.sh

--- update.sh
+++ update.sh
@@ -43,13 +43,85 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/wingshope/ngentod/main/limit/menu.zip
-    unzip menu.zip
+    #wget https://raw.githubusercontent.com/wingshope/ngentod/main/limit/menu.zip
+    #unzip menu.zip
+    #chmod +x menu/*
+    #mv menu/* /usr/local/sbin
+    #rm -rf menu
+    #rm -rf menu.zip
+    #rm -rf update.sh
+    mkdir menu
+    cd menu
+    wget -O add-bot-notif "${instalasi}menu/add-bot-notif.sh"
+    wget -O add-bot-panel "${instalasi}menu/add-bot-panel.sh"
+    wget -O addhost "${instalasi}menu/addhost.sh"
+    wget -O addss "${instalasi}menu/addss.sh"
+    wget -O addssh "${instalasi}menu/addssh.sh"
+    wget -O addtr "${instalasi}menu/addtr.sh"
+    wget -O addvless "${instalasi}menu/addvless.sh"
+    wget -O addws "${instalasi}menu/addws.sh"
+    wget -O autokill "${instalasi}menu/adutokill.sh"
+    wget -O autoreboot "${instalasi}menu/autoreboot.sh"
+    wget -O backup "${instalasi}menu/backup.sh"
+    wget -O bot "${instalasi}menu/bot.sh"
+    wget -O bw "${instalasi}menu/bw.sh"
+    wget -O cekssh "${instalasi}menu/cekssh.sh"
+    wget -O cekss "${instalasi}menu/cekss.sh"
+    wget -O cektr "${instalasi}menu/cektr.sh"
+    wget -O cekvless "${instalasi}menu/cekvless.sh"
+    wget -O cekws "${instalasi}menu/cekws.sh"
+    wget -O clearcache "${instalasi}menu/clearcache.sh"
+    wget -O clearlog "${instalasi}menu/clearlog.sh"
+    wget -O del-bot-notif "${instalasi}menu/del-bot-notif.sh"
+    wget -O delexp "${instalasi}menu/delexp.sh"
+    wget -O delss "${instalasi}menu/delss.sh"
+    wget -O delssh "${instalasi}menu/delssh.sh"
+    wget -O deltr "${instalasi}menu/deltr.sh"
+    wget -O delvless "${instalasi}menu/delvless.sh"
+    wget -O delws "${instalasi}menu/delws.sh"
+    wget -O fixcert "${instalasi}menu/fixcert.sh"
+    wget -O hapus-bot "${instalasi}menu/hapus-bot.sh"
+    wget -O limitspeed "${instalasi}menu/limitspeed.sh"
+    wget -O m-sshws "${instalasi}menu/m-sshws.sh"
+    wget -O m-ssws "${instalasi}menu/m-ssws.sh"
+    wget -O m-trojan "${instalasi}menu/m-trojan.sh"
+    wget -O m-vless "${instalasi}menu/m-vless.sh"
+    wget -O m-vmess "${instalasi}menu/m-vmess.sh"
+    wget -O mbot "${instalasi}menu/mbot.sh"
+    wget -O member "${instalasi}menu/member.sh"
+    wget -O menu-backup "${instalasi}menu/menu-backup.sh"
+    wget -O menu "${instalasi}menu/menu.sh"
+    wget -O renewss "${instalasi}menu/renewss.sh"
+    wget -O renewssh "${instalasi}menu/renewssh.sh"
+    wget -O renewtr "${instalasi}menu/renewtr.sh"
+    wget -O renewvless "${instalasi}menu/renewvless.sh"
+    wget -O renewws "${instalasi}menu/renewws.sh"
+    wget -O reset "${instalasi}menu/reset.sh"
+    wget -O restart-bot "${instalasi}menu/restart-bot.sh"
+    wget -O restart "${instalasi}menu/restart.sh"
+    wget -O restore "${instalasi}menu/restore.sh"
+    wget -O restorevmess "${instalasi}menu/restorevmess.sh"
+    wget -O run "${instalasi}menu/run.sh"
+    wget -O sd "${instalasi}menu/sd.sh"
+    wget -O stop-bot "${instalasi}menu/stop-bot.sh"
+    wget -O tendang "${instalasi}menu/tendang.sh"
+    wget -O trial "${instalasi}menu/trial.sh"
+    wget -O trialss "${instalasi}menu/trialss.sh"
+    wget -O trialtr "${instalasi}menu/trialtr.sh"
+    wget -O trialvless "${instalasi}menu/trialvless.sh"
+    wget -O trialws "${instalasi}menu/trialws.sh"
+    wget -O user-ss "${instalasi}menu/user-ss.sh"
+    wget -O user-tr "${instalasi}menu/user-tr.sh"
+    wget -O user-vless "${instalasi}menu/user-vless.sh"
+    wget -O user-ws "${instalasi}menu/user-ws.sh"
+    wget -O xp "${instalasi}menu/xp.sh"
+    cd /root
+    #wget ${instalasi}menu/menu.zip
+    #unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
     rm -rf menu
-    rm -rf menu.zip
-    rm -rf update.sh
+    #rm -rf menu.zip
 }
 netfilter-persistent
 clear
