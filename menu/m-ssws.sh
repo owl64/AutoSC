465edadaa495b4fafddce9462ffd08d35d43a2bc
owl64 <hermawandi64@gmail.com>
2023-12-25 22:28:24 +0700

    modified:   menu/m-ssws.sh 	modified:   menu/m-trojan.sh 	modified:   menu/m-vless.sh

--- menu/m-ssws.sh
+++ menu/m-ssws.sh
@@ -57,8 +57,8 @@ checking_sc() {
     echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
     echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
     echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mTelegram${NC} t.me/wingsofhope"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282261333421"
+    echo -e "      \033[0;36mTelegram${NC} t.me/owl64"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282217067357"
     echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
     exit
   fi
