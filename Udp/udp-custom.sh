8f2ddd08cc45adf0fd8e9eb653f7de2a4ce45d77
owl64 <hermawandi64@gmail.com>
2024-02-01 12:13:22 +0700

    modified:   Udp/config.json 	modified:   Udp/udp-custom.sh

--- Udp/udp-custom.sh
+++ Udp/udp-custom.sh
@@ -1,51 +1,20 @@
 #!/bin/bash
-y='\033[1;33m' #yellow
-BGX="\033[42m"
-CYAN="\033[96m"
-z="\033[96m"
-RED='\033[0;31m'
-NC='\033[0m'
-gray="\e[1;30m"
-Blue="\033[0;34m"
-green='\033[0;32m'
-grenbo="\e[92;1m"
-purple="\033[1;95m"
-YELL='\033[0;33m'
-Suffix="\033[0m"
-source /usr/local/sbin/spiner
 
-echo -e " ${z}┌─────────────────────────────────────────────┐${NC}"
-echo -e " ${z}│$NC$purple       INSTALATION UDP               $NC${z}│$NC"
-echo -e " ${z}└─────────────────────────────────────────────┘${NC}"
-echo -e ""
-start_spinner " Please wait, Make New data."
 cd
 rm -rf /root/udp
 mkdir -p /root/udp
-stop_spinner
-echo -e " ${green}Success Make Data...${Suffix}"
 
-start_spinner " Please wait, Set Time GMT"
 # change to time GMT+7
 echo "change to time GMT+7"
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
-stop_spinner
-echo -e " ${green}Success Set Time...${Suffix}"
 
-start_spinner " Please wait, Downloading UDP Server"
 # install udp-custom
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ixz82G_ruRBnEEp4vLPNF2KZ1k8UfrkV' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ixz82G_ruRBnEEp4vLPNF2KZ1k8UfrkV" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
-chmod +x /root/udp/udp-custom
-stop_spinner
-echo -e " ${green}Success Download...${Suffix}"
+echo downloading udp-custom
+wget -q -O /root/udp/udp-custom "https://raw.githubusercontent.com/owl64/AutoSC/main/Udp/udp-custom-linux-amd64" && chmod +x /root/udp/udp-custom
 
-start_spinner " Please wait, Downloading Config UDP"
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1klXTiKGUd2Cs5cBnH3eK2Q1w50Yx3jbf' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1klXTiKGUd2Cs5cBnH3eK2Q1w50Yx3jbf" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
-chmod 644 /root/udp/config.json
-stop_spinner
-echo -e " ${green}Success Download Config...${Suffix}"
+echo downloading default config
+wget -q -O /root/udp/config.json "https://raw.githubusercontent.com/owl64/AutoSC/main/Udp/config.json" && chmod 644 /root/udp/config.json
 
-start_spinner " Please wait, Set UDP...."
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
@@ -79,16 +48,9 @@ RestartSec=2s
 WantedBy=default.target
 EOF
 fi
-stop_spinner
-echo -e " ${green}Success Set UDP...${Suffix}"
 
 echo start service udp-custom
 systemctl start udp-custom &>/dev/null
 
 echo enable service udp-custom
 systemctl enable udp-custom &>/dev/null
-
-rm -rf /root/udp-custom.sh
-
-echo reboot
-reboot
