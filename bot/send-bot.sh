c11e68d2c9fb39e0726265d1fc3f489cdc55a847
owl64 <hermawandi64@gmail.com>
2024-02-08 12:01:47 +0700

    modified:   bot/send-bot.sh

--- bot/send-bot.sh
+++ bot/send-bot.sh
@@ -34,6 +34,7 @@ TEXT="
 <code>Aktif Selama     : $masaaktif Hari</code>
 <code>Dibuat Pada      : $tnggl</code>
 <code>Berakhir Pada    : $expe</code>
+<code>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</code>
 "
 curl -s --max-time $TIME -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 }
