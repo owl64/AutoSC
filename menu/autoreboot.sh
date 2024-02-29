e94bd0b92f383f44054feebfc73d0760e5baf8cc
owl64 <hermawandi64@gmail.com>
2024-02-29 13:44:27 +0700

    modified:   menu/autoreboot.sh

--- menu/autoreboot.sh
+++ menu/autoreboot.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Menu For Script
-# Edition : Stable Edition V3.0
-# Auther  : Yg Recode
-# (C) Copyright 2023-2024
-# =========================================
 BIBlack='\033[1;90m'      # Black
 BIRed='\033[1;91m'        # Red
 BIGreen='\033[1;92m'      # Green
